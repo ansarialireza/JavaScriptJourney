d902a86c488810d265f23431fa3578f7108e4869
alireza <58270983+ansarialireza@users.noreply.github.com>
2024-08-09 16:57:48 +0330

    ✅

--- assets/js/mycode.js
+++ assets/js/mycode.js
@@ -1,18 +1,25 @@
-function Hello() {
-  var l1 = document.getElementById("l1");
-  l1.innerHTML = "salam";
-}
+// function Hello() {
+//   var l1 = document.getElementById("l1");
+//   l1.innerHTML = "salam";
+// }
 
-// document.write("saaalaaammm")
-function sum() {
-  var num1 = document.getElementById("num1").value;
-  var num2 = document.getElementById("num2").value;
-  num1 = parseInt(num1);
-  num2 = parseInt(num2);
-  var res = num1 + num2;
-  var output = document.getElementById("res");
-  output.innerHTML = res;
-  console.log("salaaaam")
-}
+// // document.write("saaalaaammm")
+// function sum() {
+//   var num1 = document.getElementById("num1").value;
+//   var num2 = document.getElementById("num2").value;
+//   num1 = parseInt(num1);
+//   num2 = parseInt(num2);
+//   var res = num1 + num2;
+//   var output = document.getElementById("res");
+//   output.innerHTML = res;
+//   console.log("salaaaam")
+// }
 
-sum();
+// sum();
+// var title = "Please Enter Your name : "
+// var def = "Alireza"
+// var resault = prompt(title,[def])
+// alert(resault)
+
+let isAdmin = confirm("Are you Admin ? ")
+alert(isAdmin)
